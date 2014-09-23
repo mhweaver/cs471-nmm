fe48c4798f9d6b6f217f233c8d22701482b08330
mhweaver <mhweaver@gmail.com>
2014-09-23 12:23:56 -0600

    Added some basic funtionality to Node class

--- src/Node.java
+++ src/Node.java
@@ -1,4 +1,24 @@
 //Created By: Matthew Gillatt
 public class Node {
-	//Node stuff right
+  private Node left, right, up, down;
+  //Node stuff right
+  public Node() {
+    
+  }
+  public Node setLeft(Node left) {
+    left.setRight(this);
+    return this.left = left;
+  }
+  public Node setRight(Node right) {
+    right.setLeft(this);
+    return this.right = right;
+  }
+  public Node setUp(Node up) {
+    up.setDown(this);
+    return this.up = up;
+  }
+  public Node setDown(Node down) {
+    down.setUp(this);
+    return this.down = down;
+  }
 }
