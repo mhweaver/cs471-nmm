9f2488e9f97edbdf9a066dd462c1660f989a0494
mhweaver <mhweaver@gmail.com>
2014-09-24 13:50:33 -0600

    Switched colors to an enum, instead of int. Because enums are neat.

--- src/Player.java
+++ src/Player.java
@@ -1,15 +1,16 @@
 
 public class Player {
-  public static final int BLACK = 1, WHITE = 2;
+  public static enum Color { BLACK, WHITE }
+  public static final Color BLACK = Color.BLACK, WHITE = Color.WHITE;
   public String name;
-  public int color;
+  public Color color;
   public Player() {
     this("Unnamed player");
   }
   public Player(String name) {
     this.name = name;
   }
-  public void setColor(int color) {
+  public void setColor(Color color) {
     this.color = color;
   }
 }
