c385fbed6833245ddf47be942409af783914b2ff
zakk <zakk@Arashi>
2014-10-13 09:37:39 -0600

    commiting work so far

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -17,6 +17,7 @@ public class GUI implements ActionListener, MouseListener {
 	private String exitGameText;
 	private String whitePath;
 	private String blackPath;
+	private String selectPath;
 	private ImageIcon boardIcon;
 	private ImageIcon whiteIcon;
 	private ImageIcon blackIcon;
@@ -41,6 +42,7 @@ public class GUI implements ActionListener, MouseListener {
 		imagePath = "resources/board.png";
 		whitePath = "resources/white.png";
 		blackPath = "resources/black.png";
+		selectPath = "resources/select.png";
 		newGameText = "New Game";
 		exitGameText = "Exit";
 		boardIcon = new ImageIcon(imagePath);
@@ -72,14 +74,9 @@ public class GUI implements ActionListener, MouseListener {
 			nodeLabels[i] = new JLabel();
 			centerPanel.add(nodeLabels[i]);
 		}
-		//nodeLabel0 = new JLabel("", whiteIcon, JLabel.CENTER);
-		//nodeLabel1 = new JLabel("", blackIcon, JLabel.CENTER);
-		//centerPanel.add(nodeLabel0);
-		//centerPanel.add(nodeLabel1);
 		centerPanel.add(boardLabel);
 		boardLabel.setBounds(0,0,boardLength,boardLength);
-		//nodeLabel0.setBounds(30-nodeRadius,30-nodeRadius,nodeDiameter,nodeDiameter);
-		//nodeLabel1.setBounds(320-nodeRadius,30-nodeRadius,nodeDiameter,nodeDiameter);
+		setNodeLabelBounds();
 		boardLabel.setOpaque(true);
 		centerPanel.addMouseListener(this);
 		centerPanel.repaint();
@@ -112,7 +109,10 @@ public class GUI implements ActionListener, MouseListener {
 	}
 	
 	public void newGame() {
-		
+		for(int i=0; i<24; i++) {
+			setNodeLabelBlank(i);
+		}
+		redrawBoard();
 	}
 	
 	public void addPiece(int x, int y) {
@@ -122,14 +122,56 @@ public class GUI implements ActionListener, MouseListener {
 		
 		try {
 			game.placePiece(game.currentPlayer, n.getIndex());
-			reDrawBoard();
+			setNodeLabelBlack(n.getIndex());
+			redrawBoard();
 		} catch (IllegalMoveException e) {
 			e.printStackTrace();
 		}
 	}
 	
-	public void reDrawBoard() {
+	public void redrawBoard() {
+		centerPanel.repaint();
+	}
+	
+	public void setNodeLabelBlack(int index) {
+		nodeLabels[index].setIcon(blackIcon);
+		//nodeLabels[index] = new JLabel("",blackIcon,JLabel.CENTER);
 		
+	}
+	
+	public void setNodeLabelWhite(int index) {
+		nodeLabels[index] = new JLabel("",whiteIcon,JLabel.CENTER);
+	}
+	
+	public void setNodeLabelBlank(int index) {
+		nodeLabels[index].setIcon(whiteIcon);
+	}
+	
+	public void setNodeLabelBounds() {
+		nodeLabels[0].setBounds(30-nodeRadius,30-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[1].setBounds(320-nodeRadius,30-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[2].setBounds(610-nodeRadius,30-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[3].setBounds(126-nodeRadius,126-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[4].setBounds(320-nodeRadius,126-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[5].setBounds(514-nodeRadius,126-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[6].setBounds(223-nodeRadius,223-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[7].setBounds(320-nodeRadius,223-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[8].setBounds(417-nodeRadius,223-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[9].setBounds(30-nodeRadius,320-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[10].setBounds(126-nodeRadius,320-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[11].setBounds(223-nodeRadius,320-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[12].setBounds(417-nodeRadius,320-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[13].setBounds(514-nodeRadius,320-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[14].setBounds(610-nodeRadius,320-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[15].setBounds(223-nodeRadius,417-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[16].setBounds(320-nodeRadius,417-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[17].setBounds(417-nodeRadius,417-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[18].setBounds(126-nodeRadius,514-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[19].setBounds(320-nodeRadius,514-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[20].setBounds(514-nodeRadius,514-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[21].setBounds(30-nodeRadius,610-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[22].setBounds(320-nodeRadius,610-nodeRadius,nodeDiameter,nodeDiameter);
+		nodeLabels[23].setBounds(610-nodeRadius,610-nodeRadius,nodeDiameter,nodeDiameter);
 	}
 	
 	public void actionPerformed(ActionEvent ae) {
@@ -143,12 +185,14 @@ public class GUI implements ActionListener, MouseListener {
 		for (int i =0; i<24; i++) {
 			if(game.board.getNode(i).isInRegion(me.getX(), me.getY())) {
 				System.out.println(i);
+				addPiece(me.getX(), me.getY());
 			}
 		}
+		
 	}
 	
 	
-	
+	// Unused methods inherited from MouseEvent
 	public void mouseClicked(MouseEvent me) {	
 	}
 	public void mouseEntered(MouseEvent arg0) {		
